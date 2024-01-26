dedc9bfde3a943f6ac825a7e59635c600498c95e
massimoalbarello <massimo00albarello@gmail.com>
2024-01-26 15:40:54 +0100

    added latest block endpoint

--- src/tendermint_rpc_canister/src/lib.rs
+++ src/tendermint_rpc_canister/src/lib.rs
@@ -1,37 +1,48 @@
 use ic_cdk::api::management_canister::http_request::{
-    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse, TransformArgs,
+    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse,
     TransformContext,
 };
 mod base64string;
 mod endpoints;
 mod request;
-use endpoints::tx_sync::Request;
+use endpoints::{block::Request as BlockRequest, tx_sync::Request as TxSyncRequest};
 use request::Wrapper;
+
+#[ic_cdk::update]
+async fn latest_block() -> Result<(), String> {
+    let request = BlockRequest::default();
+    let request_body = Wrapper::new(request).into_json().into_bytes();
+
+    let response = make_rpc_request(HttpMethod::GET, Some(request_body), None).await?;
+    let str_body =
+        String::from_utf8(response.body).expect("Transformed response is not UTF-8 encoded.");
+    ic_cdk::api::print(format!("{:?}", str_body));
+
+    Ok(())
+}
 
 #[ic_cdk::update]
 async fn broadcast_tx_sync(tx_raw: String) -> Result<(), String> {
     let tx = hex::decode(tx_raw).map_err(|e| e.to_string())?;
-
-    let request = Request::new(tx);
-
+    let request = TxSyncRequest::new(tx);
     let request_body = Wrapper::new(request).into_json().into_bytes();
 
-    ic_cdk::api::print(format!("{:?}", request_body));
+    let response = make_rpc_request(HttpMethod::POST, Some(request_body), None).await?;
+    let str_body =
+        String::from_utf8(response.body).expect("Transformed response is not UTF-8 encoded.");
+    ic_cdk::api::print(format!("{:?}", str_body));
 
-    // let host = "putsreq.com";
+    Ok(())
+}
+
+async fn make_rpc_request(
+    method: HttpMethod,
+    request_body: Option<Vec<u8>>,
+    transform: Option<TransformContext>,
+) -> Result<HttpResponse, String> {
     let url = "https://rpc.sandbox-01.aksh.pw";
 
-    // 2.2 prepare headers for the system http_request call
-    //Note that `HttpHeader` is declared in line 4
     let request_headers = vec![
-        // HttpHeader {
-        //     name: "Host".to_string(),
-        //     value: format!("{host}:443"),
-        // },
-        // HttpHeader {
-        //     name: "User-Agent".to_string(),
-        //     value: "demo_HTTP_POST_canister".to_string(),
-        // },
         // //For the purposes of this exercise, Idempotency-Key" is hard coded, but in practice
         // //it should be generated via code and unique to each POST request. Common to create helper methods for this
         // HttpHeader {
@@ -47,52 +58,16 @@ async fn broadcast_tx_sync(tx_raw: String) -> Result<(), String> {
     let request = CanisterHttpRequestArgument {
         url: url.to_string(),
         max_response_bytes: None, //optional for request
-        method: HttpMethod::POST,
+        method,
         headers: request_headers,
-        body: Some(request_body),
-        transform: None,
+        body: request_body,
+        transform,
     };
-
-    ic_cdk::api::print(format!("{:?}", request));
 
     match http_request(request).await {
-        //4. DECODE AND RETURN THE RESPONSE
-
-        //See:https://docs.rs/ic-cdk/latest/ic_cdk/api/management_canister/http_request/struct.HttpResponse.html
-        Ok((response,)) => {
-            //if successful, `HttpResponse` has this structure:
-            // pub struct HttpResponse {
-            //     pub status: Nat,
-            //     pub headers: Vec<HttpHeader>,
-            //     pub body: Vec<u8>,
-            // }
-
-            //You need to decode that Vec<u8> that is the body into readable text.
-            //To do this:
-            //  1. Call `String::from_utf8()` on response.body
-            //  3. Use a switch to explicitly call out both cases of decoding the Blob into ?Text
-            let str_body = String::from_utf8(response.body)
-                .expect("Transformed response is not UTF-8 encoded.");
-            ic_cdk::api::print(format!("{:?}", str_body));
-
-            //The API response will looks like this:
-            // { successful: true }
-
-            //Return the body as a string and end the method
-            let result: String = format!(
-                "{}. See more info of the request sent at: {}/inspect",
-                str_body, url
-            );
-            result
-        }
-        Err((r, m)) => {
-            let message =
-                format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
-
-            //Return the error as a string and end the method
-            message
-        }
-    };
-
-    Ok(())
+        Ok((response,)) => Ok(response),
+        Err((r, m)) => Err(format!(
+            "The http_request resulted into error. RejectionCode: {r:?}, Error: {m}"
+        )),
+    }
 }
