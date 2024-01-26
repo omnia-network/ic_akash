5db954e7d54412eee3845d61ab86ae305dd784b7
Luca8991 <luca.berte98@hotmail.it>
2024-01-26 21:41:51 +0100

    feat: akash off-chain client with mTLS

---
+++ akash-client/index.ts
@@ -0,0 +1,55 @@
+import { loadOrCreateCertificate } from "./src/cert";
+import * as https from "https";
+
+const main = async () => {
+  const { csr, publicKey, privateKey } = await loadOrCreateCertificate("akash1435dj4zjfz59rux9akthdcf6cy7h693fte6ge2");
+
+  console.log("Cert PEM:", csr);
+  console.log("Pub Key PEM:", publicKey);
+  console.log("Priv Key PEM:", privateKey);
+
+  console.log("Cert HEX:", Buffer.from(csr).toString("hex"));
+  console.log("Pub Key HEX:", Buffer.from(publicKey).toString("hex"));
+
+  const sortedJSONManifest = "";
+  const path = `/deployment/${0}/manifest`;
+
+  const uri = new URL("https://httpbin.org/put");
+  const agent = new https.Agent({
+    cert: csr,
+    key: privateKey,
+  });
+
+  await new Promise((resolve, reject) => {
+    const req = https.request({
+      hostname: uri.hostname,
+      port: uri.port,
+      path: path,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Content-Length": sortedJSONManifest.length,
+      },
+      agent: agent,
+    }, (res) => {
+      res.on("error", reject);
+
+      res.on("data", chunk => {
+        console.log("Response:", chunk.toString());
+      });
+
+      if (res.statusCode !== 200) {
+        return reject(`Could not send manifest: ${res.statusCode}`);
+      }
+
+      resolve("ok");
+    });
+
+    req.on("error", reject);
+    req.write(sortedJSONManifest);
+    req.end();
+  });
+};
+
+main();
