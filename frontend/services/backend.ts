db020821f571cde793aff8fccf18adc32196fa41
Luca8991 <luca.berte98@hotmail.it>
2024-02-05 14:06:36 +0100

    fix: createActor function and typings

--- frontend/services/backend.ts
+++ frontend/services/backend.ts
@@ -1,23 +1,28 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 // @ts-ignore
-import { idlFactory } from "@/declarations/backend.did";
+import { type _SERVICE, idlFactory } from "@/declarations/backend.did.js";
 
 const canisterId = process.env.CANISTER_ID_BACKEND!;
 
-const agent = new HttpAgent({
-  host: process.env.NEXT_PUBLIC_IC_HOST,
-});
+const createActor = () => {
+  const agent = new HttpAgent({
+    host: process.env.NEXT_PUBLIC_IC_HOST,
+  });
 
-if (process.env.DFX_NETWORK !== "ic") {
-  agent.fetchRootKey().catch((err) => {
-    console.warn(
-      "Unable to fetch root key. Check to ensure that your local replica is running"
-    );
-    console.error(err);
+  if (process.env.DFX_NETWORK !== "ic") {
+    agent.fetchRootKey().catch((err) => {
+      console.warn(
+        "Unable to fetch root key. Check to ensure that your local replica is running"
+      );
+      console.error(err);
+    });
+  }
+
+  return Actor.createActor<_SERVICE>(idlFactory, {
+    agent,
+    canisterId,
   });
 }
 
-export const backend = Actor.createActor(idlFactory, {
-  agent,
-  canisterId,
-});
+
+export const backend = createActor();
