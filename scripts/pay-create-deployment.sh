afe3a9382df19f06e92e66d1ea02b66303975452
massimoalbarello <massimo00albarello@gmail.com>
2024-02-24 10:49:31 +0100

    fix: script closes deployment after 60 secs

--- scripts/pay-create-deployment.sh
+++ scripts/pay-create-deployment.sh
@@ -4,23 +4,15 @@ export BACKEND_PRINCIPAL=$(dfx canister id backend)
 export BACKEND_ACCOUNT_ID=$(dfx ledger account-id --of-principal $BACKEND_PRINCIPAL)
 result=$(dfx ledger transfer $BACKEND_ACCOUNT_ID --icp 5 --memo 0)
 
-echo "Result: $result"
-
 block_height=$(echo "$result" | grep -o '[0-9]*' | awk '{print $NF}')
-
 echo "Extracted Block Height: $block_height"
 
 result=$(dfx canister call backend create_test_deployment '('$block_height')')
 
-echo "Result: $result"
+deployment_id=$(echo "$result" | grep -o '"[^"]*"')
+echo "Extracted Deployment ID: $deployment_id"
 
-# Use grep to extract the line containing the ID, and cut to get the ID between ""
-deployment_id=$(echo "$result" | grep -o '"[^"]*"' | cut -d '"' -f 2)
-
-echo "Extracted ID: $deployment_id"
-
-echo "Sleeping for 60 seconds..."
+echo "Waiting 60 seconds before closing deployment..."
 sleep 60
-echo "Wake up after 60 seconds!"
 
-dfx canister call backend close_deployment '("'$deployment_id'")'
+dfx canister call backend close_deployment '('$deployment_id')'
