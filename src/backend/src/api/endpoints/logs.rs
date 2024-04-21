c4b997cb33f22aa46b8eccf6c9d0a2d388c7f50b
massimoalbarello <massimo00albarello@gmail.com>
2024-04-21 15:14:58 +0100

    chore: generalized LogController

--- src/backend/src/api/endpoints/logs.rs
+++ src/backend/src/api/endpoints/logs.rs
@@ -1,30 +1,36 @@
 use crate::api::{ApiError, ApiResult, LogServiceImpl, ListLogsResponse, LogsFilterRequest, AccessControlService, LogService, LogRepositoryImpl};
 use candid::Principal;
-use ic_cdk::*;
+use ic_cdk::{caller, query};
 
 #[query]
 fn list_logs(request: LogsFilterRequest) -> ApiResult<ListLogsResponse> {
     let calling_principal = caller();
-    
+
     LogController::default()
         .list_logs(calling_principal, request)
         .into()
 }
 
-struct LogController {
+struct LogController<L: LogService> {
     access_control_service: AccessControlService,
-    log_service: LogServiceImpl<LogRepositoryImpl>,
+    log_service: L,
 }
 
-impl LogController {
+impl Default
+    for LogController<
+        LogServiceImpl<LogRepositoryImpl>,
+    >
+{
     fn default() -> Self {
         Self::new(
             AccessControlService::default(),
             LogServiceImpl::default(),
         )
     }
+}
 
-    fn new(access_control_service: AccessControlService, log_service: LogServiceImpl<LogRepositoryImpl>) -> Self {
+impl<L: LogService> LogController<L> {
+    fn new(access_control_service: AccessControlService, log_service: L) -> Self {
         Self {
             access_control_service,
             log_service,
