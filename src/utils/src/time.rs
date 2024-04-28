bcdbfbd0345cee2d8d3273d441eaf484e6f941fc
ilbertt <luca.berte98@hotmail.it>
2024-04-28 18:42:34 +0200

    refactor: get_date_time

--- src/utils/src/time.rs
+++ src/utils/src/time.rs
@@ -1,7 +1,15 @@
 const NANOS_IN_SECOND: u64 = 1_000_000_000;
 
 pub fn get_time_nanos() -> u64 {
-    ic_cdk::api::time()
+    #[cfg(target_family = "wasm")]
+    {
+        ic_cdk::api::time()
+    }
+
+    #[cfg(not(target_family = "wasm"))]
+    {
+        1704063600_000_000_000 // 2024-01-01T00:00:00 in nanoseconds
+    }
 }
 
 pub fn get_time_seconds() -> u64 {
@@ -9,32 +17,14 @@ pub fn get_time_seconds() -> u64 {
 }
 
 pub fn get_date_time() -> Result<chrono::DateTime<chrono::Utc>, String> {
-    #[cfg(target_family = "wasm")]
-    let date_time = {
-        static NS_PER_S: u64 = 1_000_000_000;
+    let timestamp_s = get_time_seconds();
+    let timestamp_s = timestamp_s.try_into().map_err(|_| {
+        format!(
+            "Failed to convert timestamp {} from into seconds",
+            timestamp_s
+        )
+    })?;
 
-        let timestamp_ns = ic_cdk::api::time();
-        let timestamp_s: i64 = (timestamp_ns / NS_PER_S).try_into().map_err(|_| {
-            format!(
-                "Failed to convert timestamp {} from nanoseconds to seconds",
-                timestamp_ns
-            )
-        })?;
-
-        chrono::DateTime::from_timestamp(timestamp_s, 0).ok_or_else(|| {
-            format!(
-                "Failed to convert timestamp {} to DateTime",
-                timestamp_s
-            )
-        })?
-    };
-
-    #[cfg(not(target_family = "wasm"))]
-    let date_time = {
-        use chrono::{TimeZone, Utc};
-
-        Utc.with_ymd_and_hms(2021, 5, 10, 0, 0, 0).unwrap()
-    };
-
-    Ok(date_time)
+    chrono::DateTime::from_timestamp(timestamp_s, 0)
+        .ok_or_else(|| format!("Failed to convert timestamp {} to DateTime", timestamp_s))
 }
